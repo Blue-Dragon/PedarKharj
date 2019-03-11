eda88217f71696e8e4f38cf95983676b2f366942
Hamed Ganjeali <hamedganjeali@gmail.com>
2019-03-11 10:38:40 +0330

    before VolleySingleton

---
+++ app/src/main/java/com/example/pedarkharj/profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.pedarkharj.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pedarkharj.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/com/example/pedarkharj/profile/URLs.java
@@ -0,0 +1,12 @@
+package com.example.pedarkharj.profile;
+
+public class URLs {
+    /**
+     * WHY THIS CLASS?
+     * We need to define our URL that call the API of server-side.
+     */
+    private static final String ROOT_URL = "http://192.168.43.103/JavaTPoint/registrationapi.php?apicall=";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
