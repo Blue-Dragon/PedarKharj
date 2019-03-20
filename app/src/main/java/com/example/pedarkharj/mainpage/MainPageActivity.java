2e1c13e9666399780153e67441697b4b11647a64
Blue-Dragon <hamedganjeali@gmail.com>
2019-03-20 20:44:59 +0330

    Inhance Drawable: costom toolbar

--- app/src/main/java/com/example/pedarkharj/mainpage/MainPageActivity.java
+++ app/src/main/java/com/example/pedarkharj/mainpage/MainPageActivity.java
@@ -1,50 +1,35 @@
 package com.example.pedarkharj.mainpage;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.example.pedarkharj.R;
 
-public class MainPageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainPageActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer);
 
-        //drawer
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        //drawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout);
 
-        if (id == R.id.barcode) {
-            Toast.makeText(this, ""+item.getItemId(), Toast.LENGTH_SHORT).show();
+        //toggle (what does it do?)
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        } else if (id == R.id.game) {
-            Toast.makeText(this, ""+item.getItemId(), Toast.LENGTH_SHORT).show();
+        
 
-        } else if (id == R.id.about_heading) {
-            Toast.makeText(this, ""+item.getItemId(), Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.exit) {
-            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
-
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
