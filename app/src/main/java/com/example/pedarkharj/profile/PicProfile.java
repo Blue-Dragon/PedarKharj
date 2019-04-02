dad5992c4fbe412e9eeddd4f78cad19875b444f5
Hamed Ganjeali <hamedganjeali@gmail.com>
2019-04-02 17:32:06 +0430

    take profile pic from gallery or camera

--- app/src/main/java/com/example/pedarkharj/profile/PicProfile.java
+++ app/src/main/java/com/example/pedarkharj/profile/PicProfile.java
@@ -1,53 +1,62 @@
 package com.example.pedarkharj.profile;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.toolbox.StringRequest;
 import com.example.pedarkharj.R;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 
 public class PicProfile extends AppCompatActivity{
 
+    private static final int PERMISSION_CODE = 1;
+    private static final int GALLERY_INTENT = 10;
+    private static final int CAMERA_INTENT = 11;
+
     CircleImageView profPicCircle;
-    LinearLayout changePrifilePic;
+    LinearLayout profilePicLT;
     EditText usernameEdt, emailEdt, passwordEdt;
     RadioGroup radioGroupGender;
     RadioButton maleRB, femaleRB;
     Button saveBtn;
     ProgressDialog pDialog;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_prifile);
         //init
+        activity = this;
         profPicCircle = findViewById(R.id.user_profile_photo);
-        changePrifilePic = findViewById(R.id.change_pic_layout);
+        profilePicLT = findViewById(R.id.change_pic_layout);
         usernameEdt = findViewById(R.id.usrname_edit);
         passwordEdt = findViewById(R.id.password_edit);
         emailEdt = findViewById(R.id.email_edit);
@@ -84,7 +93,18 @@ public class PicProfile extends AppCompatActivity{
                 pDialog.cancel();
             });
 
-            //on profile click
+            //change profile pic
+            profilePicLT.setOnClickListener(item -> {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (!checkpermission())
+                        requestpermissions();
+                    else
+                        choosecameragallery();
+                }
+
+            });
+
+
 
 
         } else {
@@ -124,7 +144,7 @@ public class PicProfile extends AppCompatActivity{
             return;
         }
 
-        /**
+        /*
          *  you should create an updateProfile.php.file first
          */
 
@@ -182,4 +202,68 @@ public class PicProfile extends AppCompatActivity{
 //        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 
+
+    //change profile pic methods
+    private Boolean checkpermission() {
+        int chkcamera = ContextCompat.checkSelfPermission(activity, CAMERA);
+        int chkgallery = ContextCompat.checkSelfPermission(activity, READ_EXTERNAL_STORAGE);
+        return chkcamera == PackageManager.PERMISSION_GRANTED && chkgallery == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestpermissions() {
+        ActivityCompat.requestPermissions(activity, new String[]{CAMERA, READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                boolean permissioncamera = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean permissiongallery = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (grantResults.length > 0 && permissioncamera && permissiongallery) {
+                    Toast.makeText(activity, "مجوز دسترسی داده شد", Toast.LENGTH_SHORT).show();
+                    choosecameragallery();
+                } else {
+                    Toast.makeText(activity, "مجوز دسترسی داده نشد", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+
+    private void choosecameragallery() {
+        AlertDialog.Builder al = new AlertDialog.Builder(activity);
+        al.setTitle("انتخاب عکس")
+                .setMessage("لطفا موقعیت عکس خود را مشخص کنید")
+                .setPositiveButton("گالری", (dialog, which) -> {
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, GALLERY_INTENT);
+                })
+                .setNegativeButton("دوربین", (dialog, which) -> {
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_INTENT);
+                })
+                .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                profPicCircle.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Bitmap bitmap1 = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+            profPicCircle.setImageBitmap(bitmap1);
+        }
+    }
 }
