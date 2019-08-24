4728b74f8ebd926d9d91d261f8b9107a31112204
Blue-Dragon <hamedganjeali@gmail.com>
2019-08-24 17:15:27 +0430

    build calculator result field

--- app/src/main/java/com/example/pedarkharj_edit2/pages/AddExpenseActivity.java
+++ app/src/main/java/com/example/pedarkharj_edit2/pages/AddExpenseActivity.java
@@ -31,17 +31,17 @@ public class AddExpenseActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.chooseContact_RecView);
 
         contacts = new ArrayList<Contact>();
-        contacts.add(new Contact(drawableToBitmap(R.drawable.q), "hamed"));
-        contacts.add(new Contact("reza"));
-        contacts.add(new Contact("غلوم"));
+        contacts.add(new Contact(drawableToBitmap(R.drawable.w), "hamed"));
+        contacts.add(new Contact("reza dasdf dadas dasd"));
+        contacts.add(new Contact(drawableToBitmap(R.drawable.r),"غلوم"));
         contacts.add(new Contact("حسین عباس پور"));
-        contacts.add(new Contact("حامد گنجعلی"));
         contacts.add(new Contact("محمد صیدالی"));
-        contacts.add(new Contact("مری"));
         contacts.add(new Contact("پیمان"));
         contacts.add(new Contact("reza"));
+        contacts.add(new Contact(drawableToBitmap(R.drawable.r),"مری"));
         contacts.add(new Contact("غلوم"));
-        contacts.add(new Contact("حسین عbnnnnnnnnnnnnnnnnnnnباس پور"));
+        contacts.add(new Contact(drawableToBitmap(R.drawable.w),"حامد گنجعلی"));
+        contacts.add(new Contact("حسین حسی حشسیح حظسز شسزبح پور"));
         contacts.add(new Contact("حامد گنجعلی"));
         contacts.add(new Contact("محمد صیدالی"));
         contacts.add(new Contact("مری"));
