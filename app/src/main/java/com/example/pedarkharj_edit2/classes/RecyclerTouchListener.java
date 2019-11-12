5355457d260fc5579f6468fa644122bf86107d63
Blue-Dragon <hamedganjeali@gmail.com>
2019-11-12 20:57:21 +0330

    033 - ready to transfer

---
+++ app/src/main/java/com/example/pedarkharj_edit2/classes/RecyclerTouchListener.java
@@ -0,0 +1,43 @@
+package com.example.pedarkharj_edit2.classes;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * as recyclerView doesn't have the onItemClickListener method,
+ * we should create it ourselves
+ */
+public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+    private GestureDetector gestureDetector;
+    private ClickListener clickListener;
+
+    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+        this.clickListener = clickListener;
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener()) //todo
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+
+    public interface ClickListener {
+        void  onClick(View view, int position);
+        void onLongClick(View view, int position);
+    }
+
+}
